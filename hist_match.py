db321ab28e33aa4a6bc186a3af6b0e95b78ba679
Şahin Olut <olut.sahin@gmail.com>
2018-10-11 23:14:25 +0300

    add LUT calculation

--- hist_match.py
+++ hist_match.py
@@ -10,3 +10,13 @@ def calculate_cdf(bins):
     A = np.sum(bins)
     cdf = [np.sum(bins[:i]) for i in range(1, len(bins)+1)] / A
     return cdf
+
+
+def construct_lut(x, y):
+    LUT = np.zeros(256)
+    g_j = 0
+    for g_i in range(256):
+        while x[g_j] < y[g_i] and g_j < 255:
+            g_j += 1
+        LUT[g_i] = g_j
+    return LUT
